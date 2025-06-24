9d314e1646401f3fb20f4ed38bf7f5641b2cdf63
AdielGS <adielgsalviano@gmail.com>
2025-06-24 10:52:15 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciadorDeDespesasPessoais/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GerenciadorDeDespesasPessoais.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
